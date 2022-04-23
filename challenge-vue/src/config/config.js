b78920d350e3e0218880a0df46eccff1292ed300
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-04-23 12:41:08 +0200

    refactor: add FFT size comment

--- challenge-vue/src/config/config.js
+++ challenge-vue/src/config/config.js
@@ -1,6 +1,7 @@
 /**
  * Window size in samples that is used when performing a
  * Fast Fourier Transform (FFT) to get frequency domain data.
+ * Must be in 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, and 32768
  */
 const FFT_SIZE = 64;
 
